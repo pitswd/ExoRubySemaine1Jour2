1c4eda7d16aa5832f615bfcce2ec14d9a27f681e
Polo <paul.poitrenaud@gmail.com>
2019-10-01 11:33:49 +0200

    ajout exo 3

---
+++ exo_03.rb
@@ -0,0 +1,6 @@
+a = "Bonjour monde!"
+#puts a 
+puts "Et avec une voix sexy, ça donne : #{a}"
+
+#ajouter un # devant une ligne permet d'en faire un commentaire. Ainsi, la l1ère ligne disparait
+#pour ne laisser place qu'à la seconde.
